f9b3fcedc2f7885aa25c94a15ed0e46b81cabe45
Eiki Yamashiro Santos <49211526+EikiSantos@users.noreply.github.com>
2019-04-16 16:16:35 -0300

    Teste

---
+++ cor.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 10:45:29 2019
+
+@author: eikis
+"""
+
+#ESQUEMA DE CORES:
+
+print ('\033[31m'+'Isto eh vermelho'+'\033[0;0m')
+print ('\033[32m'+'Isto eh verde'+'\033[0;0m')
+print ('\033[42m'+'\033[1m'+'\033[33m'+'Isto eh amarelo negrito com fundo verde'+'\033[0;0m')
+print ('\033[42;1;33m'+'Isto eh amarelo negrito com fundo verde'+'\033[0;0m')
+
+#------------------------------------------------------------------------------------------------------------
+
+vermelho = '\033[31m'
+verde = '\033[32m'
+azul = '\033[34m'
+ciano = '\033[36m'
+magenta = '\033[35m'
+amarelo = '\033[33m'
+preto = '\033[30m'
+branco = '\033[37m'
+restaura cor original = '\033[0;0m'
+negrito = '\033[1m'
+reverso = '\033[2m'
+fundo preto = '\033[40m'
+fundo vermelho = '\033[41m'
+fundo verde = '\033[42m'
+fundo amarelo = '\033[43m'
+fundo azul = '\033[44m'
+fundo magenta = '\033[45m'
+fundo ciano = '\033[46m'
+fundo branco = '\033[47m'
